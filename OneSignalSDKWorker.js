e763c0286c3c0ad2b9edc87fc9d9c42d696fa05c
mohammedhamza99999-maker <mohammedhamza99999@gmail.com>
2026-05-06 18:15:13 +0300

    Update OneSignalSDKWorker.js

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -1,1 +1,6 @@
 importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
+
+// خدعة المتصفح حتى يعتبر موقع بطولة دوائر خانقين تطبيق رسمي ويسمح بتثبيته
+self.addEventListener('fetch', function(event) {
+  // هذا الكود فارغ وميأثر على شي، بس يخلي المتصفح ينطينا الضوء الأخضر
+});
