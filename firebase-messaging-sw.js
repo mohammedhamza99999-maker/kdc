15f4f50dfe295674bf015c916403e72f30b81a97
mohammedhamza99999-maker <mohammedhamza99999@gmail.com>
2026-05-10 14:40:45 +0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,6 +1,12 @@
-importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
+importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
 
-// خدعة المتصفح حتى يعتبر موقع بطولة دوائر خانقين تطبيق رسمي ويسمح بتثبيته
-self.addEventListener('fetch', function(event) {
-  // هذا الكود فارغ وميأثر على شي، بس يخلي المتصفح ينطينا الضوء الأخضر
+firebase.initializeApp({
+    apiKey: "AIzaSyC05TqAcGRExjLwBqwwFLrjGA-t3jI0UdQ",
+    authDomain: "kdcc-85139.firebaseapp.com",
+    databaseURL: "https://kdcc-85139-default-rtdb.firebaseio.com",
+    projectId: "kdcc-85139",
+    messagingSenderId: "743548860238",
+    appId: "1:743548860238:web:66b66fe49cf32da352101f"
 });
+const messaging = firebase.messaging();
